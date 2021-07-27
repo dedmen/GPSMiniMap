b521fedfefccc07ba32c23af970b7f5ace9ae01f
Dedmen Miller <dedmen@dedmen.de>
2021-07-27 14:28:14 +0200

    Added obs websocket Changed to .NET 5

--- GPSMinimapReceiver/Program.cs
+++ GPSMinimapReceiver/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using OBSWebsocketDotNet;
+using OBSWebsocketDotNet.Types;
 using System;
 
 namespace GPSMinimapReceiver
@@ -17,9 +20,7 @@ namespace GPSMinimapReceiver
                .WithAutomaticReconnect()
                .Build();
 
-            hubConnection.On<string>("ChatMessage", (message) => {
-                Console.WriteLine($"CHAT {DateTime.Now:T} {message}");
-            });
+    
 
             hubConnection.On<string>("UpdatePosition", (message) => {
                 Console.WriteLine($"POS {DateTime.Now:T} {message}");
@@ -30,6 +31,50 @@ namespace GPSMinimapReceiver
                 (x) => {
                     Console.WriteLine($"Connected? State: {hubConnection.State}");
                 });
+
+
+            OBSWebsocket obs = new OBSWebsocket();
+
+            obs.Connected += (sender, type) => { Console.WriteLine($"OBS websocket connected!"); };
+            obs.StreamingStateChanged += (sender, type) => { Console.WriteLine($"OBS Streaming status changed: {type}"); };
+            obs.StreamStatus += (sender, status) => { Console.WriteLine($"OBS Streaming status: { JsonConvert.SerializeObject(status)}"); };
+
+
+            // connect to OBS
+
+            try
+            {
+                obs.Connect("ws://127.0.0.1:4444", "");
+            }
+            catch (AuthFailureException)
+            {
+                Console.WriteLine($"OBS Authentication failed.");
+            }
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine($"OBS Connect failed : {ex.Message}");
+            }
+
+
+
+            hubConnection.On<string>("ChatMessage", (message) => {
+                Console.WriteLine($"CHAT {DateTime.Now:T} {message}");
+
+                if (message == "switchCamera")
+                {
+                    // Hide Browser 2
+                    var oldProps = obs.GetSceneItemProperties("Browser 2");
+                    oldProps.Visible = false;
+                    obs.SetSceneItemProperties(oldProps);
+
+                    // Show "Fensteraufnahme"
+                    oldProps = obs.GetSceneItemProperties("Fensteraufnahme");
+                    oldProps.Visible = true;
+                    obs.SetSceneItemProperties(oldProps);
+                }
+
+            });
+
 
 
             bool wantExit = false;
