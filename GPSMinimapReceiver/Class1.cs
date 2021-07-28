ea9c4a5572c926eba4a8f187374d53951a20b5b8
jackd23 <d.mon@gmx.net>
2021-07-28 21:22:56 +0200

    new features yay

---
+++ GPSMinimapReceiver/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPSMinimapReceiver
+{
+    class Class1
+    {
+    }
+}
---
+++ GPSMinimapReceiver/Secret.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPSMinimapReceiver
+{
+    class Secret
+    {
+        public static string HubLink {  get { return "https://minimap.dedmen.de/chathub"; } }
+    }
+}
