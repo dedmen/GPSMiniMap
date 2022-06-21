70e664ed26f166a3dfce1e851eb4963496fef76e
Dedmen Miller <dedmen@dedmen.de>
2022-06-21 16:11:05 +0200

    Remember last position and send instantly when new client connects

--- GPSMiniMap/Hubs/ChatHub.cs
+++ GPSMiniMap/Hubs/ChatHub.cs
@@ -6,10 +6,15 @@ namespace GPSMiniMap.Hubs
 {
     public class ChatHub : Hub
     {
+        private static string lastPositionMessage = "";
+        private static DateTime lastPositionTime = DateTime.MinValue;
+
         public async Task UpdatePosition(string message)
         {
             //await Clients.All.SendAsync("UpdatePosition", message); //#TODO others only
             await Clients.Others.SendAsync("UpdatePosition", message);
+            lastPositionTime = DateTime.Now;
+            lastPositionMessage = message;
         }
 
         public async Task ChatMessage(string message)
@@ -20,6 +25,10 @@ namespace GPSMiniMap.Hubs
         public override Task OnConnectedAsync()
         {
             Console.WriteLine($"{DateTime.Now:s} User connected {Context.ConnectionId}");
+
+            if (lastPositionTime.AddMinutes(5) > DateTime.Now)
+                Clients.Caller.SendAsync("UpdatePosition", lastPositionMessage);
+
             return base.OnConnectedAsync();
         }
 
